9f65abd57eae54248a6f32969393adf407bca8ba
Raziq7 <raziqsur@gmail.com>
2022-02-21 11:41:45 +0530

    last commit

--- app.js
+++ app.js
@@ -3,17 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var { create } = require ('express-handlebars');
+var { create } = require('express-handlebars');
 const { handlebars } = require('hbs');
 const DB = require('./config/connect')
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
 var app = express();
 var fileUpload = require('express-fileupload');
-let Session =require('express-session')
+const dotenv = require('dotenv')
+let Session = require('express-session')
+const MongoStore = require('connect-mongo');
+
+dotenv.config()
 
 
-app.use(Session({secret:"key",cookie:{maxAge:600000}}))
+app.use(Session({
+  secret: "key",
+  store: MongoStore.create({ mongoUrl: `mongodb+srv://Raziq:${process.env.MONGO_PASS}@zahraf.bb5ow.mongodb.net/zahraf?retryWrites=true&w=majority` }),
+  cookie: {
+    maxAge: 600000
+  }
+}))
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -33,8 +43,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 
-DB.connect((err)=>{
-  if(err) console.log('Database not connectes',err)
+DB.connect((err) => {
+  if (err) console.log('Database not connectes', err)
   else console.log("data base connected");
 })
 
@@ -42,12 +52,12 @@ app.use('/', usersRouter);
 app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
